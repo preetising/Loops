6783714262c063569c628ec0efeffaf9a9d6b884
preetising <84845068+preetising@users.noreply.github.com>
2021-10-10 13:42:36 +0530

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,8 +1,131 @@
+# row=6
+# for i in range(row+1):
+#     for i in range(1,i):
+#         print(i,end="")
+#     print()
+
+
+
+
+# row =5
+# for a in range(row,1,-1):
+#     for a in range(1,a):
+#         print(a,end="")
+#     print()
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(i,end="")
+#         j=j+1
+#     i=i+1
+#     print()
+
+
+# a=input("enter the any character...")
+# b='ly'
+# c='ing'
+# if a>=3:
+#     print('a is greater')
+# elif  a>3:
+#     print("3 is  greater"+a+c)
+
+
+
+
+# if True:
+#   print("Hello Python If")
+# if 2 < 1:
+#   print("2 is greater than 1")
+# else:
+#     print("both are equal")
+
+
+# i=5
+# while i>1:
+#     j=1
+#     while j<=i:
+#         print(j,end="")
+#         j=j+1
+#     i=i+1
+#     print()
+
+
+# i=25
+# while i>1:
+#     j=25
+#     while j<=i:
+#         print(i,end="")
+#         j=j+1
+#     i=i+1
+#     print()
+#     break
+
+
+# a=int(input("enter the number..."))
+# for i in range(a):
+#     for j in range(i+1):
+#         print(a-i,end="")
+#     print()
+
+
+# a=int(input("ente rthe number..."))
+# for i in range(a+1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(i,end=' ')
+#         j=j+1
+#     i=i+1
+#     print()
+
+# i=1
+# while i>4:
+#     j=4
+#     while j>1:
+#         print("*",end='')
+#         j=j+1
+#     i=i+1
+#     print()
+
+
+# i=1
+# while i<=4:
+#     j=1
+#     while j<=i:
+#         print("*",end="")
+#         j=j+1
+#     i=i+1
+#     print()
+
+
+# r=5
+# for i in range(r+1):
+#     for j in range(i-1):
+#         print(r,end="")
+#     print()
+
+
+# i=20
+# while i<=1:
+#     j=1
+#     while j<=i:
+#         print(i,end=" " )
+#         j=j+1
+#     i=i+1
+    #print()
+
+a=input("enter the alphabte...")
 i=1
-while i<=5:
-	j=1
-	while j<=5:
-		print(j,end=" ")
-		j=j+1
-	print()
-	i=i+1
+while i<62:
+    j=0
+    
+
